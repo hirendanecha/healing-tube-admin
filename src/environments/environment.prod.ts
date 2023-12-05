a1dac54157d5d912c1c74c123693a02440a43449
Swapnil Opash <sra.opash@gmail.com>
2023-12-05 15:32:28 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  serverUrl: 'https://freedom-api.opash.in/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://freedom-api.opash.in/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+
+};
