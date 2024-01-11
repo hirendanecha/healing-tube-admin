b2f09324a5865ec5596ae9f9f60d2e93cbb63219
vatsal <vso.opash@gmail.com>
2024-01-11 17:04:13 +0530

    Add appointmnet model and fix text

--- src/app/views/community-post/community-post.module.ts
+++ src/app/views/community-post/community-post.module.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommunityPostComponent } from './community-posts.component';
 import { CommunityPostRoutingModule } from './community-post-routing.module';
-import { SharedModule } from 'src/app/@shared/shared.module';
+import { SharedModule } from '../../@shared/shared.module';
 import { ViewCommunityPostComponent } from '../community-post/view-post/edit-post.component';
 @NgModule({
   declarations: [CommunityPostComponent, ViewCommunityPostComponent],
